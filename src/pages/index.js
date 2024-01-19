fb64d2367c30203af443e63cedf6d84fa281017d
Vlad <racerun@posteo.uk>
2024-01-19 10:13:36 +0000

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -127,7 +127,7 @@ const IndexPage = () => {
   return (
     <main style={pageStyles}>
       <h1 style={headingStyles}>
-        Congratulations
+        Привет пёс!
         <br />
         <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
       </h1>
