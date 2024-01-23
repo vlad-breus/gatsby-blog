355bcf07e531d8888144d3b210cb3d190e188c5d
Vlad <racerun@posteo.uk>
2024-01-23 16:23:48 +0000

    try fixing case sensitivity issue - attempt 2

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import Layout from '../../components/layout';
 import Seo from '../../components/seo';
 import { graphql } from 'gatsby';
-import PostGrid from '../../components/postGrid';
+import PostGrid from '../../components/PostGrid';
 
 const BlogPage = ({ data }) => {
   return (
